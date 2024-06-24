b540297b6b1e9d4c9e3d49cfc6226604b03ce773
Ali Şahan Yalçın <alisahanyalcin4@gmail.com>
2024-06-24 15:39:02 +0300

    Refactor AI_Zone to AIZone and add custom editor for target tag management
    Renamed AI_Zone.cs to AIZone.cs to follow C# naming conventions.
    Removed the unnecessary try-catch block in ClosestObject method.
    Added a custom editor script (AIZoneEditor.cs) to allow easy selection of the target tag in the Unity Inspector.
    Improved variable and method names for better readability and maintainability.

---
+++ AIZone.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class AIZone : MonoBehaviour
+{
+    private List<GameObject> _targetList = new List<GameObject>();
+	public GameObject closesObj;
+	[HideInInspector] public string targetTag;
+
+    private void Update()
+    {
+        closesObj = ClosestObject();
+		if (closesObj != null )
+		{
+			Debug.Log(closesObj.name);
+		}	
+    }
+    
+    public GameObject ClosestObject()
+    {
+	    if (!_targetList.Any())
+	    {
+		    Debug.LogWarning("No targets in the list");
+		    return null;
+	    }
+
+	    return _targetList.OrderBy(obj => Vector3.Distance(transform.position, obj.transform.position)).First();
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag(targetTag))
+		{
+			AddTarget(other.gameObject);
+		}
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag(targetTag))
+		{			
+			DeleteTarget(other.gameObject);
+		}
+	}
+
+	public void AddTarget(GameObject target)
+    {
+	    _targetList.Add(target);
+        Debug.Log(target.name + " Added to the list");
+		
+	}
+	
+    public void DeleteTarget(GameObject target)
+    {
+	    _targetList.Remove(target);
+		Debug.Log(target.name + " Removed from the list");		
+	}
+}
---
+++ Editor/AIZoneEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    [CustomEditor(typeof(AIZone))]
+    public class AIZoneEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            AIZone aiZone = (AIZone) target;
+            aiZone.targetTag = EditorGUILayout.TagField("Target Tag", aiZone.targetTag);
+        }
+    }
+}
